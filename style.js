64fafc40773616e6cc8cfd9d71781dc8a45e348d
KamauDev-maker <oscarnjenga@gmail.com>
2022-03-06 11:11:45 +0300

    extracting data from input data

--- style.js
+++ style.js
@@ -1,6 +1,13 @@
 function calculateBirthday(){
-    const fullName=document.getElementsByName('fname').value;
-    const genDer=document.getElementsByName('gender').value;
-    const dateOfBirth=document.getElementById('date_of_birth').value;
+    const fullName=document.getElementById("full_name").value;
+    const myGender=document.getElementById("gender").value;
+    const dateOfBirth=document.getElementById("date_of_birth").value;
+
+    //extract data 
+
+    let full_name=new name(fullName);
+    let gender= new genDer(myGender);
+    let startDate=new Date=(dateOfBirth);
+
 
 }
