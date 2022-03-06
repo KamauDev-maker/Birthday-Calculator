b3a2cbafad7ba23147ab56592a42cf4d75388964
KamauDev-maker <oscarnjenga@gmail.com>
2022-03-06 09:40:55 +0300

    getting the element by id

--- style.js
+++ style.js
@@ -1,4 +1,6 @@
 function calculateBirthday(){
     const fullName=document.getElementsByName('fname').value;
-    const genDer=document.getElementsByName('gender').value
+    const genDer=document.getElementsByName('gender').value;
+    const dateOfBirth=document.getElementById('date_of_birth').value;
+
 }
