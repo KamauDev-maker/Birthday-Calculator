6962600fde779547775317cc92bcb8917a03a85a
KamauDev-maker <oscarnjenga@gmail.com>
2022-03-06 14:58:13 +0300

    creating the function

--- style.js
+++ style.js
@@ -1,41 +1,64 @@
-function calculateBirthday(){
-    const fullName=document.getElementById("full_name").value;
-    const myGender=document.getElementById("gender").value;
-    const dateOfBirth=document.getElementById("date_of_birth").value;
 
-    //extract data
-
-    let cc= year/100;
-    yy=year-((year/100)*100);
-
-    c=(cc/4)-2*cc-1;
-    y= 5*yy/4;
-    m=26*(month+1)/10;
-    d= day;
-
-    dayofweek =(c+y+m+d)%7;
-
-    switch(dayofweek){
-        case 0: dayString="Sunday";
-        break;
-        case 1: dayString="Monday";
-        break;
-        case 2: dayString="Tuseday";
-        break;
-        case 3: dayString="Wednesday";
-        break;
-        case 4: dayString="Thursday";
-        break;
-        case 5: dayString="Friday";
-        break;
-        case 6: dayString="Saturday";
-        break;
+function MakeArray(n) {
+    this.length = n;
+    for (var i = 1; i <=n; i++) {
+      this[i] = 0;
     }
-    let textAnswer=document.createTextNode('The day is' + dayofweek);
-
-    return dayString;
-
-
-
-
-}
+ 
+ 
+ days = new MakeArray(7);
+ days[0] = "Saturday"
+ days[1] = "Sunday"
+ days[2] = "Monday"
+ days[3] = "Tuesday"
+ days[4] = "Wednesday"
+ days[5] = "Thursday"
+ days[6] = "Friday"
+ 
+ months = new MakeArray(12);
+ months[1] = "January" 
+ months[2] = "February" 
+ months[3] = "March" 
+ months[4] = "April" 
+ months[5] = "May" 
+ months[6] = "June" 
+ months[7] = "July" 
+ months[8] = "August"
+ months[9] = "September" 
+ months[10] = "October"
+ months[11] = "November"
+ months[12] = "December"
+ 
+ 
+ function compute(form) {
+    var val1 = parseInt(form.day.value, 10)
+    if ((val1 < 0) || (val1 > 31)) {
+       alert("Day is out of range")
+    }
+    var val2 = parseInt(form.month.value, 10)
+    if ((val2 < 0) || (val2 > 12)) {
+       alert("Month is out of range")
+    }  
+    var val2x = parseInt(form.month.value, 10)
+    var val3 = parseInt(form.year.value, 10)
+    if (val3 < 1900) {
+       alert("You're that old!")
+    }
+    if (val2 == 1) {
+       val2x = 13;
+       val3 = val3-1
+    }
+    if (val2 == 2) {
+       val2x = 14;
+       val3 = val3-1
+    }
+    var val4 = parseInt(((val2x+1)*3)/5, 10)
+    var val5 = parseInt(val3/4, 10)
+    var val6 = parseInt(val3/100, 10)
+    var val7 = parseInt(val3/400, 10)
+    var val8 = val1+(val2x*2)+val4+val3+val5-val6+val7+2
+    var val9 = parseInt(val8/7, 10)
+    var val0 = val8-(val9*7)
+    form.result1.value = months[val2]+" "+form.day.value +", "+form.year.value
+    form.result2.value = days[val0]
+}   
