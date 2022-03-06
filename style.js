1d5413289c3b28fb066038e1209b09c9ef340b0b
KamauDev-maker <oscarnjenga@gmail.com>
2022-03-06 11:28:02 +0300

    creating a day string for day week

--- style.js
+++ style.js
@@ -3,11 +3,38 @@ function calculateBirthday(){
     const myGender=document.getElementById("gender").value;
     const dateOfBirth=document.getElementById("date_of_birth").value;
 
-    //extract data 
+    //extract data
 
-    let full_name=new name(fullName);
-    let gender= new genDer(myGender);
-    let startDate=new Date=(dateOfBirth);
+    let cc= year/100;
+    yy=year-((year/100)*100);
+
+    c=(cc/4)-2*cc-1;
+    y= 5*yy/4;
+    m=26*(month+1)/10;
+    d= day;
+
+    dayofweek =(c+y+m+d)%7;
+
+    switch(dayofweek){
+        case 0: dayString="Sunday";
+        break;
+        case 1: dayString="Monday";
+        break;
+        case 2: dayString="Tuseday";
+        break;
+        case 3: dayString="Wednesday";
+        break;
+        case 4: dayString="Thursday";
+        break;
+        case 5: dayString="Friday";
+        break;
+        case 6: dayString="Saturday";
+        break;
+    }
+
+    return dayString;
+
+
 
 
 }
