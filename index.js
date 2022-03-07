a2484b0892edbf06b63babcd992b43680c4de651
KamauDev-maker <oscarnjenga@gmail.com>
2022-03-07 15:54:10 +0300

    rewritting the html

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ var CC, YY, MM, DD, d, dayValue;
 var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
-var outPut = document.getElementById("result")
-var dateOfBirth =document.getElementById("year");
-console.log(dateOfBirth);
 
 function validate() {
   var genders = document.getElementsByName("gender");
